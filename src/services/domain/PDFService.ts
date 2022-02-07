e351322d7da5b10c10a4a9d7df1e2e604c32e631
Cristian Mos <cmo@nemetos.com>
2022-02-07 19:35:12 +0200

    feat(pdfservice): generating with proper data

--- src/services/domain/PDFService.ts
+++ src/services/domain/PDFService.ts
@@ -13,22 +13,77 @@ export default class PDFService {
     const pdfDoc = await PDFDocument.create();
 
     const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const heleveticaBold = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
 
     const page = await pdfDoc.addPage();
 
-    const { height } = page.getSize();
-    const fontSize = 30;
+    const { width, height } = page.getSize();
+    const fontSize = 14;
     try {
       page.drawText(`Company - ${params.company}`, {
-        x: 50,
-        y: height - 4 * fontSize,
+        x: width / 2 - 100,
+        y: height - 10 * fontSize,
         size: fontSize,
         font: helveticaFont,
         color: rgb(0, 0, 0)
       });
 
+      page.drawText(`Siren: ${params.siren}`, {
+        x: 40,
+        y: height - 20 * fontSize,
+        size: fontSize,
+        font: helveticaFont,
+        color: rgb(0, 0, 0)
+      });
+
+      page.drawText(`IBAN: ${params.iban}`, {
+        x: 40,
+        y: height - 23 * fontSize,
+        size: fontSize,
+        font: helveticaFont,
+        color: rgb(0, 0, 0)
+      });
+      page.drawText(`VAT number: ${params.vat}`, {
+        x: 40,
+        y: height - 25 * fontSize,
+        size: fontSize,
+        font: helveticaFont,
+        color: rgb(0, 0, 0)
+      });
+
+
+      let milestoneIndex = 0;
+      let total = 0;
+      for (const index in params.milestones) {
+        milestoneIndex = Number(index) * 3;
+        const milestone = params.milestones[index];
+        total += Number(milestone.price);
+        page.drawText(`${milestone.name}`, {
+          x: 40,
+          y: height - (milestoneIndex + 28) * fontSize,
+          size: fontSize,
+          font: helveticaFont,
+          color: rgb(0, 0, 0)
+        });
+
+        page.drawText(`${milestone.price} €`, {
+          x: width - 100,
+          y: height - (milestoneIndex + 28) * fontSize,
+          size: fontSize,
+          font: heleveticaBold,
+          color: rgb(0, 0, 0)
+        });
+      }
+      page.drawText(`Total ${total} €`, {
+        x: width - 150,
+        y: height - (milestoneIndex + 35) * fontSize,
+        size: fontSize,
+        font: heleveticaBold,
+        color: rgb(0, 0, 0)
+      });
+
+
       const pdfBytes = await pdfDoc.save();
-      // @ts-ignore
       const pdfBuffer = Buffer.from(pdfBytes.buffer);
       return pdfBuffer;
     } catch (e) {
